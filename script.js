35ea3dfd79b65adce3c627b8e98d407d1c9163f2
jonath-caik <caikjonath@gmail.com>
2025-05-10 17:51:44 -0300

    brincadeira

---
+++ script.js
@@ -0,0 +1,18 @@
+function mostrarResposta() {
+  document.getElementById("resposta").style.display = "block";
+  document.getElementById("resposta").innerHTML = "SABIA, SEU FÃ INCUBADO! KKKKK";
+  document.getElementById("no").style.display = "none";
+  document.getElementById("resposta-img").style.display = "block";
+  var img = document.getElementById("resposta-img");
+  img.src =
+    "https://i.pinimg.com/736x/72/d7/f9/72d7f9a252d94df8b9656214838449d0.jpg";
+  img.height = 300;
+}
+
+function mudarPosicao() {
+  const buttonNao = document.getElementById("no");
+  const randomX = Math.floor(Math.random() * window.innerWidth);
+  const randomY = Math.floor(Math.random() * window.innerHeight);
+  buttonNao.style.left = `${randomX}px`;
+  buttonNao.style.top = `${randomY}px`;
+}
