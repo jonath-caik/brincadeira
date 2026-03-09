f8f1c57841e9a5dd915e33df6a10f3b26e5c43dc
jonath caik <caikmilitar@gmail.com>
2026-03-09 08:40:31 -0300

    atualização

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function mostrarResposta() {
   document.getElementById("resposta").style.display = "block";
-  document.getElementById("resposta").innerHTML = "vigia crente, vai orar kakakakka";
-  document.getElementById("no").style.display = "none";
+  document.getElementById("resposta").innerHTML = "se você concordou significa que você É kakakakka";
+  document.getElementById("No").style.display = "none";
   document.getElementById("resposta-img").style.display = "block";
   var img = document.getElementById("resposta-img");
   img.src =
@@ -10,7 +10,7 @@ function mostrarResposta() {
 }
 
 function mudarPosicao() {
-  const buttonNao = document.getElementById("no");
+  const buttonNao = document.getElementById("No");
   const randomX = Math.floor(Math.random() * window.innerWidth);
   const randomY = Math.floor(Math.random() * window.innerHeight);
   buttonNao.style.left = `${randomX}px`;
